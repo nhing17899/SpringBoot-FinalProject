fc0ac59923f665815330f47b1b180c7c8db9c436
loganarnett1 <59770778+loganarnett1@users.noreply.github.com>
2024-04-06 19:44:26 -0400

    Repository updates

--- src/main/java/com/todolist/enterprise/dao/TodoListRepository.java
+++ src/main/java/com/todolist/enterprise/dao/TodoListRepository.java
@@ -1,15 +1,14 @@
 package com.todolist.enterprise.dao;
 
-import java.util.List;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.todolist.enterprise.dto.Task;
+import com.todolist.enterprise.dto.TodoList;
 
 @Repository
 @Profile("!test")
-public interface TaskRepository extends CrudRepository<Task, Integer>{
-    List<Task> findByListId(int todoListId);
+public interface TodoListRepository extends CrudRepository<TodoList, Integer>{
+
 }
