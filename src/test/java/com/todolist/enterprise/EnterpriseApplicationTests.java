6a25598e7141da597d7fa63ea0268e539afaee04
Arian Kharazmi <ariankharazmi@gmail.com>
2024-04-14 20:04:21 -0400

    Added a few last minute QoL changes. Added delete button for Task Lists, and added a few EnterpriseAppTests for CI/CD test fulfillment.

--- src/test/java/com/todolist/enterprise/EnterpriseApplicationTests.java
+++ src/test/java/com/todolist/enterprise/EnterpriseApplicationTests.java
@@ -2,12 +2,56 @@ package com.todolist.enterprise;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.todolist.enterprise.dto.TodoList;
+import com.todolist.enterprise.service.ITodoService;
+import org.junit.jupiter.api.Assertions;
+import com.todolist.enterprise.service.TodoService;
+import java.util.List;
 
 @SpringBootTest
 class EnterpriseApplicationTests {
 
+
+	@Autowired
+	private ITodoService todoService;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testCreateTodoList() {
+		TodoList todoList = new TodoList();
+		todoList.setName("Test TodoList");
+		todoList.setDescription("Test Description");
+
+		todoService.createTodoList(todoList);
+
+	}
+
+	@Test
+	void testDeleteTodoList() {
+		TodoList todoList = new TodoList();
+		todoList.setName("Test TodoList");
+		todoList.setDescription("Test Description");
+
+		todoService.createTodoList(todoList);
+
+		todoService.deleteTodoList(todoList.getListId());
+
+		List<TodoList> todoLists = todoService.getTodoLists();
+		Assertions.assertFalse(todoLists.contains(todoList));
+
+	}
+
+	@Test
+	void testGetTodoLists() {
+		TodoList todoList1 = new TodoList();
+		todoList1.setName("ToDoList test number 1");
+		todoList1.setDescription("ToDoList Description 1");
+
+		List<TodoList> todoLists = todoService.getTodoLists();
 	}
 
 }
